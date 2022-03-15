b5e84ddfcec4448dbc61ec822ace4ea4b0a256e9
ELucas04 <101639155+ELucas04@users.noreply.github.com>
2022-03-15 13:46:42 -0300

    Update Regras.py

--- Regras.py
+++ Regras.py
@@ -2,17 +2,13 @@ from time import sleep
 delay = 0.7
 
 def funcao():
-    from time import sleep
-    
+    from time import sleep    
     l = ['-=' * 15]
-
     for i in l:
         print(i)
         sleep(delay)
-
-
+        
 def continuacao(cont):
-    
     if cont == 'S':
         pass
     else:
@@ -20,10 +16,8 @@ def continuacao(cont):
         quit()
     return cont    
 
-
 def rules(r):
     erro = False
-
     while not erro:
         if r == '0':
             funcao()
@@ -34,14 +28,11 @@ def rules(r):
             print("-Se as tentativas se esgotarem e o usuário voltar a errar, o usuário perde.")
             funcao()
             print("Para passar de nível o usuário terá que acertar 70% das questões. Caso contrário terá de recomeçar.")
-
             erro = True
-
         else:
             sleep(delay)
             print("\033[1;31mDígito incorreto! Digite novamente.\033[0;0m")
             sleep(delay)
-
             regras = input("Digite '0' para ver as regras: ").strip()
             if regras == '0':
                 funcao()
@@ -52,5 +43,4 @@ def rules(r):
                 print("-Se as tentativas se esgotarem e o usuário voltar a errar, o usuário perde.")
                 funcao()
                 print("Para passar de nível o usuário terá que acertar 70% das questões. Caso contrário terá de recomeçar.")
-
                 erro = True
